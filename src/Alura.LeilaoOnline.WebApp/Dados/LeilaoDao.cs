c7db3788434fec32ea87b6768adf3bc5d5caeddb
Henrique Wolff Lobo <henquewl@hotmail.com>
2023-05-01 16:55:09 -0300

    Refatoração dos controllers de leilão aplicando responsabilidade única.

--- src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -1,11 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Alura.LeilaoOnline.WebApp.Models;
 
 namespace Alura.LeilaoOnline.WebApp.Dados
 {
     public class LeilaoDao
     {
+
+        AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+        public IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria)
+                .ToList();
+        }
+
+        public Leilao BuscarLeilao(int leilao)
+        {
+            return _context.Leiloes.Find(leilao);
+        }
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Alterar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+
+        public Leilao BuscaPorId(int id)
+        {
+            return _context.Leiloes.First(l => l.Id == id);
+        }
     }
 }
